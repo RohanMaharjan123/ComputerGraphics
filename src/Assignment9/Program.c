1e6df41af773e80989c4a2f2e362fec48fec0d45
Rohan Maharjan <nahor.uzamaki@gmail.com>
2024-03-24 23:35:19 +0545

    fixes

--- src/Assignment9/Program.c
+++ src/Assignment9/Program.c
@@ -9,14 +9,39 @@
 
 int main(){
     int gd = DETECT, gm;
-    initgraph(&gd, &gm, NULL);
-    int x1,y1,x2,y2;
+    int x1,y1,x2,y2, p, i, dx, dy, m;
+    initgraph(&gd, &gm, "C:\\TURBOC3\\BGI");
     printf("Enter the coordinates of the first point\n");
     scanf("%d%d",&x1,&y1);
     printf("Enter the coordinates of the second point\n");
     scanf("%d%d",&x2,&y2);
-    line(x1,y1,x2,y2);
-    getch();
-    closegraph();
-    return 0;
-}
+    dx=x2-x1;
+    dy=y2-y1;
+    m= dy/dx;
+    if(m <= 1){
+        p = 2 * dy - dx;
+        for(i = 0 ; i < dx; i++){
+            putpixel(x1+i,y1,WHITE);
+            if(p < 0){
+                x1 = x1 + 1;
+                p = p + 2 * dy;
+            }
+            }else{
+                x1 = x1 + 1;
+                y1 = y1 + 1;
+                p = p + 2 * dy - 2 * dx;
+            }else{
+                p = 2 * dx - dy;
+                putpixel(x1, y1, RED);
+                if(p < 0){
+                    y1 = y1 + 1;
+                    p = p + 2 * dx;
+                }else{
+                    y1 = y1 + 1;
+                    x1 = x1 + 1;
+                    p += (2 * dx) - (2 * dy);
+                }
+            }
+        }
+        getch();
+    }
